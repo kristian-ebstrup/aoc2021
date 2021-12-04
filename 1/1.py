88be2f2baa28cab666431750db1dfc78b53f7f7b
TeddySilk <89346063+TeddySilk@users.noreply.github.com>
2021-12-04 20:57:46 +0100

    aoc2021 - day 1

---
+++ 1/1.py
@@ -0,0 +1,14 @@
+filename = "input"
+
+count = 0
+line_count = 0
+previous_line = 0
+with open(filename) as f:
+    for line in f.readlines():
+        line = int(line)
+        if previous_line < line and line_count > 0:
+            count += 1
+        previous_line = line
+        line_count += 1
+
+print(f"{count} increases out of {line_count} entries")
---
+++ 1/2.py
@@ -0,0 +1,10 @@
+filename = "input"
+
+measurements = []
+with open(filename) as f:
+    for line in f.readlines():
+        measurements.append(int(line))
+
+count = sum([1 for i in range(len(measurements)) if sum(measurements[i+1:i+4]) > sum(measurements[i:i+3])])
+
+print(f"{count} increases out of {len(measurements)} entries")
