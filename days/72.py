fddb95aaca218cde5cdd1c719d3876f7e7f547ce
Kristian Ebstrup Jacobsen <k.ebstrup@gmail.com>
2021-12-07 08:17:19 +0100

    Day 7 - Note that part 2 is brute forcing, and can take a bit of time.

---
+++ days/72.py
@@ -0,0 +1,19 @@
+#!usr/bin/env python
+import numpy as np
+
+filename = './inputs/day7.txt'
+
+# extract horizontal crab positions
+pos_crabs = []
+with open(filename) as f:
+    for line in f.readlines():
+        pos_crabs.append(line)
+
+pos_crabs = [int(pos) for pos in pos_crabs[0].split(',')]
+
+# run from minimum crab position to max crab position and compute the fuel costs
+fuel_cost = []
+for pos_align in range(min(pos_crabs), max(pos_crabs)):
+    fuel_cost.append(sum([sum([i for i in range(1, abs(pos - pos_align) + 1)]) for pos in pos_crabs]))
+
+print(f"The best position is found to be at hor = {np.argmin(fuel_cost)}, with minimum fuel cost estimated to be {min(fuel_cost)}")
